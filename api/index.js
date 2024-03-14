ddbd1ac102510bc9ee278fd50a0a8dfacd71dd70
Vishal Giri <vishalgiri197@gmail.com>
2024-03-14 16:29:13 +0530

    conneted to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const connection = require("./db")
 const app =express();
 
 
 
-app.listen(3000,()=>{
-   console.log("listening on http://localhost:3000");
+app.listen(3000,async()=>{
+
+   try {
+      await connection;
+      console.log("listening on http://localhost:3000");
+      console.log("connected to mongodb ")
+   } catch (error) {
+      
+   }
+   
 })
