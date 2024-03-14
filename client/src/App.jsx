b3345f49bf007e17f8096170a1e1130addf1933b
Vishal Giri <vishalgiri197@gmail.com>
2024-03-14 15:59:07 +0530

    Header_added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About"
 import Profile from "./pages/Profile"
 import Signin from "./pages/Signin"
 import SignUp from "./pages/SignUp"
+import Header from "./Components/Header"
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
