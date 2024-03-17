dcad9def96a0aa8b2ccba2cd5523e81b3cc6efa3
Vishal Giri <vishalgiri197@gmail.com>
2024-03-17 10:27:07 +0530

    frontend_code_done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Signin from "./pages/Signin"
-import SignUp from "./pages/SignUp"
-import Header from "./Components/Header"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header from './components/Header';
+import PrivateRoute from './Components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
+import Search from './pages/Search';
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
 
-      <Route path="/profile" element={<Profile/>} />
-
-      <Route path="/signin" element={<Signin/>} />
-
-      <Route path="/signup" element={<SignUp/>} />
-
-    </Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
-export default App
